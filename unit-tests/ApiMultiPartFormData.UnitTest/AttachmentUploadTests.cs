4390000245560a47d315812be050db0d86ad11aa
Linh Nguyen <redplane_dt@yahoo.com.vn>
2020-01-03 17:54:22 +0700

    - Added nested attachment upload test case.

--- unit-tests/ApiMultiPartFormData.UnitTest/AttachmentUploadTests.cs
+++ unit-tests/ApiMultiPartFormData.UnitTest/AttachmentUploadTests.cs
@@ -69,5 +69,44 @@ namespace ApiMultiPartFormData.UnitTest
             Assert.AreEqual(student.Photo.ContentType, contentType);
 
         }
+
+        [Test]
+        public void UploadStudentWithProfilePhoto_Returns_StudentWithProfilePhoto()
+        {
+            var fileName = $"{Guid.NewGuid():D}.jpg";
+            var contentType = "image/jpeg";
+
+            var logger = new Mock<IFormatterLogger>();
+            logger.Setup(x => x.LogError(It.IsAny<string>(), It.IsAny<Exception>()));
+
+            var multipartFormDataFormatter = new MultipartFormDataFormatter();
+            var multipartFormContent = new MultipartFormDataContent("---wwww-wwww-wwww-boundary-----");
+
+            var applicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var attachmentPath = Path.Combine(applicationPath, "Data", "grapefruit-slice-332-332.jpg");
+            var data = File.ReadAllBytes(attachmentPath);
+
+            var attachment = new ByteArrayContent(data);
+            attachment.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            multipartFormContent.Add(attachment, $"{nameof(StudentViewModel.Profile)}[{nameof(ProfileViewModel.Attachment)}]", fileName);
+
+            var uploadedModel = multipartFormDataFormatter
+                .ReadFromStreamAsync(typeof(StudentViewModel), new MemoryStream(),
+                multipartFormContent, logger.Object)
+                .Result;
+
+            if (!(uploadedModel is StudentViewModel student))
+            {
+                Assert.IsInstanceOf<StudentViewModel>(uploadedModel);
+                return;
+            }
+
+            Assert.NotNull(student.Profile);
+            Assert.NotNull(student.Profile.Attachment);
+            Assert.AreEqual(student.Profile.Attachment.ContentLength, data.Length);
+            Assert.AreEqual(student.Profile.Attachment.FileName, fileName);
+            Assert.AreEqual(student.Profile.Attachment.ContentType, contentType);
+
+        }
     }
 }
